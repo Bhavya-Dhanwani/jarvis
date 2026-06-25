b99c4d6ad305328f066b13581d6504419d4b6cbf
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-06-25 16:59:34 +0530

    Initialized the server

---
+++ signalingServer/server.js
@@ -0,0 +1,19 @@
+// Importing modules
+import createApp from "./src/app.js";
+import env from "./src/shared/config/env.config.js";
+
+// function to start the server
+function startServer() {
+
+    // making the app 
+    const app = createApp();
+
+    // starting the server
+    app.listen(env.PORT, () => {
+        console.log(`Server started on http://localhost:${env.PORT}`);
+    })
+
+}
+
+// starting the server
+startServer();
---
+++ signalingServer/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+function createApp() {
+
+    // making the app
+    const app = express();
+
+    // returning the app
+    return app;
+
+}
+
+export default createApp;
